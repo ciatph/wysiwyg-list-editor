c9eadc4b23d696852a4b4420fc30a56963e02bed
ciatph <ciat.ph@gmail.com>
2022-12-11 22:48:49 +0800

    chore: Set-up eslint standard on server, #1

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    // browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2022
+  },
+  rules: {
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    // 'no-console': ['error', { 'allow': ['error'] }]
+    // 'no-unused-vars': 'off',
+    // 'no-undef': 'off'
+  }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -18,3 +18,4 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`)
 })
+
